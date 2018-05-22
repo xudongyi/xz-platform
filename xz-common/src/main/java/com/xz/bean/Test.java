25e5ff8425bfc7875aa55105402a1447faf64da5
xudy <1922448115@qq.com>
2018-05-22 08:57:10 +0800

    Initial commit

---
+++ xz-common/src/main/java/com/xz/bean/Test.java
@@ -0,0 +1,14 @@
+package com.xz.bean;
+
+/**
+ * @author: xudy
+ * @date: 2018/05/21 15:44
+ * @description:
+ */
+public class Test {
+    @Override
+    public String toString() {
+        System.out.println("heheheeh");
+        return super.toString();
+    }
+}
---
+++ xz-manager/xz-manager-dao/src/main/java/Test1.java
@@ -0,0 +1,14 @@
+import com.xz.bean.Test;
+
+/**
+ * @author: xudy
+ * @date: 2018/05/21 15:44
+ * @description:
+ */
+public class Test1 {
+    Test test = null;
+    public static void main(String[] args) {
+        Test test = new Test();
+    }
+
+}
