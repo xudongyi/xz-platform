14f19521d4d97ac5375c60f6e443ed1d38c831fa
xudy <1922448115@qq.com>
2018-05-22 10:45:03 +0800

    mybatis-plus

---
+++ xz-web/src/main/java/mapper/system/UserMapper.java
@@ -0,0 +1,15 @@
+package mapper.system;
+
+
+import common.SuperMapper;
+import model.system.User;
+
+/**
+ *
+ * User 表数据库控制层接口
+ *
+ */
+public interface UserMapper extends SuperMapper<User> {
+
+
+}
